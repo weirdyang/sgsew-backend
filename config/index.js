bc93e469957458da0cc800118ff6c15613cdd3d7
weirdyang <wy4786@gmail.com>
2021-08-05 22:58:27 +0800

    feat(auth):separating keys for user registration and jwt

--- config/index.js
+++ config/index.js
@@ -1,4 +1,4 @@
 module.exports = {
-  secret: process.env.NODE_ENV === 'production' ? process.env.SECRET : 'secret',
+  secret: process.env.NODE_ENV === 'production' ? process.env.USERKEY : 'secret',
   PORT: 4000,
 };

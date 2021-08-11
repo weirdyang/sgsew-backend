5ada71534397a31fd5ca829a1ecfc83d851aa2d6
weirdyang <wy4786@gmail.com>
2021-08-12 04:31:48 +0800

    feat(image): adding image route that doesn't require auth

--- routes/search.route.js
+++ routes/search.route.js
@@ -2,8 +2,11 @@ const express = require('express');
 
 const router = express.Router();
 
+const { fetchProductImage } = require('../controllers/products.controller');
 const { searchProducts } = require('../controllers/search.controller');
 
 router.get('/products', searchProducts);
+
+router.get('/image/:id', fetchProductImage);
 
 module.exports = router;
